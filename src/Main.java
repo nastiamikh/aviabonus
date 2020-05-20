2cab5ef329d9d4a799f9c18a4563f653e21ad245
Nastia Mikh <nastiamikh@mail.ru>
2020-05-21 02:54:15 +0800

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int cost = 10_000;
-        int miles = service.calculate(cost);
+        int miles = service.calculate(100000);
         System.out.println(miles);
     }
 }
